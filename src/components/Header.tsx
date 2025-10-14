0fae87daa676c614c2ac560bd051c9d99905b67c
Movieguy3333 <Movieguy3333@gmail.com>
2025-10-14 10:16:03 -0400

    started context

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,21 @@
-import Button from './UI/Button.tsx';
+import { useContext } from "react";
+import Button from "./UI/Button.tsx";
+import { useTimersContext } from "../store/timers-context.tsx";
 
 export default function Header() {
+  const timersCtx = useTimersContext();
+
+  // timersCtx can't really be null since value prop
+  // in the provider will always have a value.
+  // However, Typescript doesn't know that so
+  // it will complain, hence, the below code.
+  /*  if (timersCtx === null)
+    throw new Error("Something went wrong") */
   return (
     <header>
       <h1>ReactTimer</h1>
 
-      <Button>Stop Timers</Button>
+      <Button> {timersCtx.isRunning ? "Stop" : "Start"}</Button>
     </header>
   );
 }
---
+++ src/store/timers-context.tsx
@@ -0,0 +1,41 @@
+import { createContext, useContext, type ReactNode } from "react";
+
+type Timer = {
+  name: string;
+  duration: number;
+};
+type TimersState = {
+  isRunning: boolean;
+  timers: Timer[];
+};
+
+type TimersContextValue = TimersState & {
+  addTimer: (timerData: Timer) => void;
+  startTimers: () => void;
+  stopTimers: () => void;
+};
+const TimersContext = createContext<TimersContextValue | null>(null);
+
+export function useTimersContext() {
+  const timersCtx = useContext(TimersContext);
+  if (timersCtx === null)
+    throw new Error("TimersContext is null - that should no be the case!");
+  return timersCtx;
+}
+type TimersContextProviderProps = {
+  children: ReactNode;
+};
+export default function TimersContextProvider({
+  children,
+}: TimersContextProviderProps) {
+  const ctx: TimersContextValue = {
+    timers: [],
+    isRunning: false,
+    addTimer(timerData) {},
+    startTimers() {},
+    stopTimers() {},
+  };
+  return (
+    <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
+  );
+}
